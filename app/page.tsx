5a3e607fa73c3d084462681cf61036260bfd5858
arilibbetardanaputra <arilardana111@gmail.com>
2026-01-19 12:00:23 +0700

    feat: enhance authentication page layout and functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+import { Play } from "lucide-react";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-    <div>
-      <Link href="/auth">
-        <button>
-          <p>test</p>
-        </button>
-      </Link>
-    </div>
+      <div className="pt-5 pl-5 mt-10 text-[#9a6c4c]">
+        <Link href="/auth">
+          <button className="className= flex items-center text-l font-medium hover:underline mb-4">
+            <Play /> back
+          </button>
+        </Link>
+      </div>
     </div>
   );
 }
